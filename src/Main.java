6cc31d3a1da67945729a6b547571e597fd1d5b05
Ruslan Karimov <98585383+IamKarimovich@users.noreply.github.com>
2024-12-02 16:18:19 +0400

    Threads for java roadmap.

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ public class Main {
     public static void main(String[] args) {
         Thread t1 = new Thread(new MyThread());
         t1.start();
+
         System.out.println(t1.getState());
         System.out.println(t1.getName());
+        System.out.println(t1.getPriority());
+
         Thread t2 = new Thread(new MyThread());
         t2.start();
+
         System.out.println(t2.getName());
         System.out.println(t2.getPriority());
-        System.out.println(t1.getPriority());
+
     }
 }
